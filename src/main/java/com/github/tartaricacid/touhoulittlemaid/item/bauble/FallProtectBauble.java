28c2561f4233494fb89d5da621004b0fd731e81f
tartaric_acid <baka943@qq.com>
2024-10-20 00:07:22 +0800

    同步继续完善成就系统 - 未完成

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/item/bauble/FallProtectBauble.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/item/bauble/FallProtectBauble.java
@@ -1,8 +1,10 @@
 package com.github.tartaricacid.touhoulittlemaid.item.bauble;
 
+import com.github.tartaricacid.touhoulittlemaid.advancements.maid.TriggerType;
 import com.github.tartaricacid.touhoulittlemaid.api.bauble.IMaidBauble;
 import com.github.tartaricacid.touhoulittlemaid.api.event.MaidDamageEvent;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+import com.github.tartaricacid.touhoulittlemaid.init.InitTrigger;
 import com.github.tartaricacid.touhoulittlemaid.network.NetworkHandler;
 import com.github.tartaricacid.touhoulittlemaid.network.message.SpawnParticleMessage;
 import com.github.tartaricacid.touhoulittlemaid.util.ItemsUtil;
@@ -29,6 +31,9 @@ public class FallProtectBauble implements IMaidBauble {
                 stack.hurtAndBreak(1, maid, m -> maid.sendItemBreakMessage(stack));
                 maid.getMaidBauble().setStackInSlot(slot, stack);
                 NetworkHandler.sendToNearby(maid, new SpawnParticleMessage(maid.getId(), SpawnParticleMessage.Type.EXPLOSION));
+                if (maid.getOwner() instanceof ServerPlayer serverPlayer) {
+                    InitTrigger.MAID_EVENT.trigger(serverPlayer, TriggerType.USE_PROTECT_BAUBLE);
+                }
             }
         }
     }
