941fd49712bca182dc3b0f05007e346f906abc38
tartaric_acid <baka943@qq.com>
2024-11-03 12:22:48 +0800

    修正与 t88 加载顺序导致的问题

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/sodium/SodiumCompat.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/sodium/SodiumCompat.java
@@ -14,16 +14,14 @@ public final class SodiumCompat {
 
     private static boolean SODIUM_INSTALLED;
     private static boolean EMBEDDIUM_INSTALLED;
-    private static RenderLevelStageEvent.Stage CUTOUT_RENDER_STAGE;
 
     public static void init() {
         SODIUM_INSTALLED = LoadingModList.get().getModFileById(SODIUM_ID) != null;
         EMBEDDIUM_INSTALLED = LoadingModList.get().getModFileById(EMBEDDIUM_ID) != null;
-        CUTOUT_RENDER_STAGE = isInstalled() ? RenderLevelStageEvent.Stage.AFTER_CUTOUT_MIPPED_BLOCKS_BLOCKS : RenderLevelStageEvent.Stage.AFTER_PARTICLES;
     }
 
     public static RenderLevelStageEvent.Stage getCutoutRenderStage() {
-        return CUTOUT_RENDER_STAGE;
+        return isInstalled() ? RenderLevelStageEvent.Stage.AFTER_CUTOUT_MIPPED_BLOCKS_BLOCKS : RenderLevelStageEvent.Stage.AFTER_PARTICLES;
     }
 
     public static boolean isInstalled() {
